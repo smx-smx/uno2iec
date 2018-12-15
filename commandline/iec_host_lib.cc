756d9857f5b6f6562baee7cdc0d614b40bd7553d
Andreas Eckleder <andreas@eckleder.de>
2018-12-15 17:45:40 +0100

    Some serial port config code

--- commandline/iec_host_lib.cc
+++ commandline/iec_host_lib.cc
@@ -1,6 +1,104 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
 #include "iec_host_lib.h"
 
-bool SendCommand(const std::string& cmd_string, IECStatus* status) {
-  // TODO(weirdsoul): Add some actual code here.
-  return true;
+static void SetError(IECStatus::IECStatusCode status_code,
+                     const std::string& context, IECStatus* status) {
+  status->status_code = status_code;
+  switch (status_code) {
+    case IECStatus::OK:
+      status->message = "OK";
+      break;
+    case IECStatus::UNIMPLEMENTED:
+      status->message = "Unimplemented";
+      break;
+    case IECStatus::CONNECTION_FAILURE:
+      status->message = "Connection failure";
+      break;
+  }
+  if (!context.empty()) {
+    status->message = context + ": " + status->message;
+  }
+}
+
+static void SetErrorFromErrno(IECStatus::IECStatusCode status_code, const std::string& context,
+                              IECStatus* status) {
+  SetError(status_code, context, status);
+  status->message = status->message + ": " + strerror(errno);
+}
+
+IECBusConnection::IECBusConnection(int arduino_fd) : arduino_fd_(arduino_fd) {}
+
+IECBusConnection::~IECBusConnection() {
+  if (arduino_fd_ != -1) {
+    close(arduino_fd_);
+    arduino_fd_ = -1;
+  }
+}
+
+bool IECBusConnection::Reset(IECStatus* status) {
+  SetError(IECStatus::UNIMPLEMENTED, "Reset", status);
+  return false;
+}
+
+bool IECBusConnection::SendCommand(int device_number,
+                                   const std::string& cmd_string,
+                                   IECStatus* status) {
+  SetError(IECStatus::UNIMPLEMENTED, "SendCommand", status);
+  return false;
+}
+
+IECBusConnection* IECBusConnection::Create(int arduino_fd, IECStatus* status) {
+  if (arduino_fd == -1) {
+    return nullptr;
+  }
+  return new IECBusConnection(arduino_fd);
+}
+
+IECBusConnection* IECBusConnection::Create(const std::string& device_file,
+                                           int speed, IECStatus* status) {
+  int fd = open(device_file.c_str(), O_RDWR | O_NONBLOCK);
+  if (fd == -1) {
+    SetErrorFromErrno(IECStatus::CONNECTION_FAILURE,
+                      "open(\"" + device_file + "\")", status);
+    return nullptr;
+  }
+  struct termios tty;
+  if (tcgetattr(fd, &tty) == -1) {
+    SetErrorFromErrno(IECStatus::CONNECTION_FAILURE, "tcgetattr", status);
+    return nullptr;
+  }
+  cfsetospeed(&tty, (speed_t)speed);
+  cfsetispeed(&tty, (speed_t)speed);
+
+  tty.c_cflag |= (CLOCAL | CREAD); /* ignore modem controls */
+  tty.c_cflag &= ~CSIZE;
+  tty.c_cflag |= CS8;      /* 8-bit characters */
+  tty.c_cflag &= ~PARENB;  /* no parity bit */
+  tty.c_cflag &= ~CSTOPB;  /* only need 1 stop bit */
+  tty.c_cflag &= ~CRTSCTS; /* no hardware flowcontrol */
+
+  /* setup for non-canonical mode */
+  tty.c_iflag &=
+      ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+  tty.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+  tty.c_oflag &= ~OPOST;
+
+  /* fetch bytes as they become available */
+  tty.c_cc[VMIN] = 1;
+  tty.c_cc[VTIME] = 1;
+
+  if (tcsetattr(fd, TCSANOW, &tty) == -1) {
+    SetErrorFromErrno(IECStatus::CONNECTION_FAILURE, "tcsetattr", status);
+    return nullptr;
+  }
+  return Create(fd, status);
 }
--- commandline/iec_host_lib.h
+++ commandline/iec_host_lib.h
@@ -8,11 +8,54 @@
 #include <string>
 
 struct IECStatus {
+  enum IECStatusCode {
+    OK = 0x00,
+    UNIMPLEMENTED = 0x01,
+    CONNECTION_FAILURE = 0x02
+  };
+  IECStatusCode status_code;
   std::string message;  // A status message describing the status.
 };
 
-// Send a command via the command channel. Returns true on success. In case of
-// an error, status will be set to an appropriate error status.
-bool SendCommand(const std::string& cmd_string, IECStatus* status);
+class IECBusConnection {
+ public:
+  // Reset the IEC bus by pulling the reset line to low. Returns true on
+  // success. In case of an error, status will be set to an appropriate error
+  // status.
+  bool Reset(IECStatus* status);
+
+  // Send a command via the command channel. to the device specified by
+  // device_number. Returns true on success. In case of an error, status will be
+  // set to an appropriate error status.
+  bool SendCommand(int device_number, const std::string& cmd_string,
+                   IECStatus* status);
+
+  // Create IECBusConnection instance using the specified device_file and serial
+  // port speed. Returns nullptr in case of a problem and sets status.
+  // Otherwise, ownership of the IECBusConnection instance is transferred to the
+  // caller.
+  static IECBusConnection* Create(const std::string& device_file, int speed,
+                                  IECStatus* status);
+
+  // Create IECBusConnection instance based on the specified arduino_fd, which
+  // must me ready to use. Ownership of the file description is passed to the
+  // IECBusConnection instance. Returns nullptr in case of a problem and sets
+  // status. Otherwise, ownership of the IECBusConnection instance is
+  // transferred to the caller.
+  static IECBusConnection* Create(int arduino_fd, IECStatus* status);
+
+  // Free resources such as any owned file descriptors.
+  virtual ~IECBusConnection();
+
+ private:
+  // Instantiate an IECBusConnection object. The arduino_fd parameter is
+  // used to specify a file descriptor that will be used for bidirectional
+  // communication with an arduino connected to the IEC bus and speaking the
+  // uno2iec protocol.
+  IECBusConnection(int arduino_fd);
+
+  // File descriptor used for communication.
+  int arduino_fd_;
+};
 
 #endif  // IEC_HOST_LIB_H

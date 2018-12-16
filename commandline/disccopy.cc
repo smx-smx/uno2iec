3b87addc7da91b052ab66725969d48f91bb0c8a6
Andreas Eckleder <andreas@eckleder.de>
2018-12-16 19:45:25 +0100

    More communication infrastructure

--- commandline/disccopy.cc
+++ commandline/disccopy.cc
@@ -1,9 +1,45 @@
-#include <stdio.h>
+#include <iostream>
+
+#include "boost/program_options/cmdline.hpp"
+#include "boost/program_options/options_description.hpp"
+#include "boost/program_options/parsers.hpp"
+#include "boost/program_options/variables_map.hpp"
+#include "iec_host_lib.h"
+
+namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
-  printf(
-      "IEC Bus disc copy utility.\n"
-      "Copyright (c) 2018 Andreas Eckleder\n");
+  std::cout << "IEC Bus disc copy utility." << std::endl
+            << "Copyright (c) 2018 Andreas Eckleder" << std::endl
+            << std::endl;
+
+  std::string arduino_device;
+  int serial_speed = 0;
+
+  po::options_description desc("Options");
+  desc.add_options()("help", "usage overview")(
+      "serial",
+      po::value<std::string>(&arduino_device)->default_value("/dev/ttyUSB0"),
+      "serial interface to use")(
+      "speed", po::value<int>(&serial_speed)->default_value(115200),
+      "baud rate");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return 1;
+  }
+
+  IECStatus status;
+  std::unique_ptr<IECBusConnection> connection(
+      IECBusConnection::Create(arduino_device, serial_speed, &status));
+  if (!connection) {
+    std::cout << status.message << std::endl;
+    return 1;
+  }
 
   return 0;
 }
--- commandline/iec_host_lib.cc
+++ commandline/iec_host_lib.cc
@@ -1,14 +1,42 @@
 #include <fcntl.h>
-#include <memory>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
+#include <iostream>
+#include <memory>
 
+#include "boost/format.hpp"
 #include "iec_host_lib.h"
+
+// Maximum chars to read looking for '\r'. This is intentionally larger than
+// kConnectionStringPrefix, because it is followed by a version number.
+static const size_t kMaxLength = 256;
+
+static const std::string kConnectionStringPrefix = "connect_arduino:";
+
+// TODO(aeckleder): We'll need to increase the version as soon as we start
+// supporting custom features.
+static const int kMinProtocolVersion = 2;
+
+// Number of tries for successfully reading the connection string prefix.
+static const int kNumRetries = 5;
+
+// Config values. These are hardcoded for now and match the defaults of
+// the Arduino implementation.
+// TODO(aeckleder): We'll be playing the host, so we shouldn't specify a device
+// number.
+static const int kDeviceNumber = 8;
+
+static const int kAtnPin = 5;
+static const int kDataPin = 3;
+static const int kClockPin = 4;
+static const int kSrqInPin = 6;
+static const int kResetPin = 7;
 
 IECBusConnection::IECBusConnection(int arduino_fd) : arduino_fd_(arduino_fd) {}
 
@@ -32,8 +60,58 @@ bool IECBusConnection::SendCommand(int device_number,
 }
 
 bool IECBusConnection::Initialize(IECStatus* status) {
-  // const char kConnectionStringPrefix[] = "connect_arduino:";
-  return false;
+  std::string connection_string;
+  for (int i = 0; i < kNumRetries; ++i) {
+    if (!ReadTerminatedString(arduino_fd_, '\r', kMaxLength, &connection_string,
+                              status)) {
+      return false;
+    }
+    if (connection_string.size() >= kConnectionStringPrefix.size() &&
+        connection_string.substr(0, kConnectionStringPrefix.size()) ==
+            kConnectionStringPrefix) {
+      break;
+    } else if (i >= (kNumRetries - 1)) {
+      SetError(
+          IECStatus::CONNECTION_FAILURE,
+          std::string("Unknown protocol response: '") + connection_string + "'",
+          status);
+      return false;
+    }
+  }
+  int protocol_version = 0;
+  if (sscanf(connection_string.substr(kConnectionStringPrefix.size()).c_str(),
+             "%i", &protocol_version) <= 0 ||
+      protocol_version < kMinProtocolVersion) {
+    SetError(
+        IECStatus::CONNECTION_FAILURE,
+        std::string("Unknown protocol response: '") + connection_string + "'",
+        status);
+    return false;
+  }
+  time_t unix_time = time(nullptr);
+  struct tm local_time;
+  localtime_r(&unix_time, &local_time);
+
+  // Now talk back to the Arduino, communicating our configuration.
+  auto config_string =
+      boost::format("OK>%u|%u|%u|%u|%u|%u|%u-%u-%u.%u:%u:%u\r") %
+      kDeviceNumber % kAtnPin % kClockPin % kDataPin % kResetPin % kSrqInPin %
+      (local_time.tm_year + 1900) % (local_time.tm_mon + 1) %
+      local_time.tm_mday % local_time.tm_hour % local_time.tm_min %
+      local_time.tm_sec;
+  if (!WriteString(arduino_fd_, config_string.str(), status)) return false;
+
+  // TODO(aeckleder): We won't have to do this in the future. But right now,
+  // we're trying to understand the existing protocol.
+  for (int i = 0; i < 6; ++i) {
+    std::string log_string;
+    if (!ReadTerminatedString(arduino_fd_, '\r', kMaxLength, &log_string,
+                              status)) {
+      return false;
+    }
+    std::cout << "LOG LINE: " << log_string << std::endl;
+  }
+  return true;
 }
 
 IECBusConnection* IECBusConnection::Create(int arduino_fd, IECStatus* status) {
@@ -86,4 +164,3 @@ IECBusConnection* IECBusConnection::Create(const std::string& device_file,
   }
   return Create(fd, status);
 }
-
